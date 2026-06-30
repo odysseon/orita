622db78ed0709c1f81e0ebdc53234d251b78a7e3
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-06-30 09:07:27 +0100

    refactor(routes): move business feature to be a child of profile and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './pages/auth/auth.routes';
 import { profileRoutes } from './pages/profile/profile.routes';
-import { businessRoutes } from './pages/business/business.routes';
 import { homeRoutes } from './pages/home/home.routes';
 import { landingRoutes } from './pages/landing/landing.route';
 import { businessDetailRoutes } from './pages/business-detail/business-detail.routes';
 import { listingDetailRoutes } from './pages/listing-detail/listing-detail.routes';
 export const routes: Routes = [
   ...authRoutes,
-  ...businessRoutes,
   ...businessDetailRoutes,
   ...homeRoutes,
   ...landingRoutes,
--- src/app/pages/profile/business/business.routes.ts
+++ src/app/pages/profile/business/business.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { authGuard } from '../../core/guards/auth.guard';
+import { authGuard } from '../../../core/guards/auth.guard';
 import { Business } from './business';
 import { CreateBusiness } from './create/create-business';
 
 export const businessRoutes: Routes = [
   {
-    path: 'business',
+    path: 'profile/business',
     canActivate: [authGuard],
     component: Business,
   },
   {
-    path: 'business/create',
+    path: 'profile/business/create',
     canActivate: [authGuard],
     component: CreateBusiness,
   },
