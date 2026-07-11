8270ea8877116037fda542517af340f92cac6991
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-07-11 19:28:17 +0100

    chore(media): extract cloudinary upload base url to environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   googleClientId: '235860880185-ht64nu13sbg36r8squbpb12pngf7tan7.apps.googleusercontent.com',
+  cloudinaryUploadBaseUrl: 'https://api.cloudinary.com/v1_1',
 };
