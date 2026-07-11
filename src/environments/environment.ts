06155e3749ab2bd8f638756bb7ca6249c7faf813
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-07-11 20:00:17 +0100

    feat(auth): fetch Google Client ID dynamically from backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://inquisitive-ernestine-odysseon-ae88add1.koyeb.app/api',
-  googleClientId: '235860880185-ht64nu13sbg36r8squbpb12pngf7tan7.apps.googleusercontent.com',
   cloudinaryUploadBaseUrl: 'https://api.cloudinary.com/v1_1',
 };
