04b9fc27bd771097b8d88c631cfd760392acfe3a
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-08-03 16:50:03 +0100

    feat: introduce dedicated send button for message composer

---
+++ src/app/shared/ui/actions/send-button/send-button.ts
@@ -0,0 +1,34 @@
+import { Component, input, output } from '@angular/core';
+import { Button, ButtonAppearance, ButtonShape, ButtonSize } from '../../atoms/button/button';
+import { LucideSend } from '@lucide/angular';
+
+@Component({
+  selector: 'ui-send-button',
+  standalone: true,
+  imports: [Button, LucideSend],
+  template: `
+    <button app-button 
+      [appearance]="appearance()"
+      [shape]="shape()" 
+      [size]="size()"
+      [disabled]="disabled()"
+      (click)="send.emit()"
+      type="button"
+      aria-label="Send">
+      <svg lucideSend aria-hidden="true" style="width: 18px; height: 18px;"></svg>
+    </button>
+  `,
+  styles: [`
+    :host {
+      display: contents;
+    }
+  `]
+})
+export class SendButton {
+  appearance = input<ButtonAppearance>('ghost');
+  shape = input<ButtonShape>('circle');
+  size = input<ButtonSize>('md');
+  disabled = input<boolean>(false);
+  
+  send = output<void>();
+}
